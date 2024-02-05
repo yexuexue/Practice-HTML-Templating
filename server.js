327ccff09436c684002dba744e564384018c077d
yexuexue <withyexuexue@qq.com>
2024-02-06 00:51:07 +0800

    Practice HTML Templating

--- server.js
+++ server.js
@@ -1,94 +1,102 @@
-const http = require('http');
-const fs = require("fs");
+const http = require('http')
+const fs = require('fs')
 
 const dogs = [
   {
     dogId: 1,
     name: 'Fido',
-    age: 2
+    age: 2,
   },
   {
     dogId: 2,
     name: 'Fluffy',
-    age: 10
-  }
-];
+    age: 10,
+  },
+]
 
-let nextDogId = 3;
+let nextDogId = 3
 
 function getNewDogId() {
-  const newDogId = nextDogId;
-  nextDogId++;
-  return newDogId;
+  const newDogId = nextDogId
+  nextDogId++
+  return newDogId
 }
 
-
 function getContentType(fileName) {
-  const ext = fileName.split(".")[1];
+  const ext = fileName.split('.')[1]
   switch (ext) {
-    case "jpg":
-    case "jpeg":
-      return "image/jpeg";
-    case "png":
-      return "image/png";
-    case "css":
-      return "text/css";
+    case 'jpg':
+    case 'jpeg':
+      return 'image/jpeg'
+    case 'png':
+      return 'image/png'
+    case 'css':
+      return 'text/css'
     default:
-      return "text/plain";
+      return 'text/plain'
   }
 }
 
 const server = http.createServer((req, res) => {
-  console.log(`${req.method} ${req.url}`);
-  if (req.method === "GET" && req.url.startsWith('/assets')) {
-    const assetPath = req.url.split("/assets")[1];
+  console.log(`${req.method} ${req.url}`)
+  if (req.method === 'GET' && req.url.startsWith('/assets')) {
+    const assetPath = req.url.split('/assets')[1]
     try {
-      const resBody = fs.readFileSync("./assets" + assetPath);
-      res.statusCode = 200;
-      res.setHeader("Content-Type", getContentType(assetPath));
-      res.write(resBody);
-      return res.end();
+      const resBody = fs.readFileSync('./assets' + assetPath)
+      res.statusCode = 200
+      res.setHeader('Content-Type', getContentType(assetPath))
+      res.write(resBody)
+      return res.end()
     } catch {
-      console.error("Cannot find asset", assetPath, "in assets folder");
+      console.error('Cannot find asset', assetPath, 'in assets folder')
     }
   }
 
-  let reqBody = "";
-  req.on("data", (data) => {
-    reqBody += data;
-  });
+  let reqBody = ''
+  req.on('data', (data) => {
+    reqBody += data
+  })
 
   // When the request is finished processing the entire body
-  req.on("end", () => {
+  req.on('end', () => {
     // Parsing the body of the request
     if (reqBody) {
       req.body = reqBody
-        .split("&")
-        .map((keyValuePair) => keyValuePair.split("="))
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+        .split('&')
+        .map((keyValuePair) => keyValuePair.split('='))
+        .map(([key, value]) => [key, value.replace(/\+/g, ' ')])
         .map(([key, value]) => [key, decodeURIComponent(value)])
         .reduce((acc, [key, value]) => {
-          acc[key] = value;
-          return acc;
-        }, {});
-      console.log(req.body);
+          acc[key] = value
+          return acc
+        }, {})
+      console.log(req.body)
     }
 
     // route handlers
     // GET /
     if (req.method === 'GET' && req.url === '/') {
-      const htmlPage = fs.readFileSync("./views/index.html", 'utf-8');
-      const resBody = htmlPage;
+      const htmlPage = fs.readFileSync('./views/index.html', 'utf-8')
+      const resBody = htmlPage
 
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.write(resBody);
-      return res.end();
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/html')
+      res.write(resBody)
+      return res.end()
     }
 
     // Phase 1: GET /dogs
     if (req.method === 'GET' && req.url === '/dogs') {
       // Your code here
+      const htmlDogsTemplate = fs.readFileSync('./views/dogs.html', 'utf-8')
+
+      const dogsListHtml = dogs.map((dog) => `<li>${dog.name}</li>`).join('')
+
+      const htmlDogs = htmlDogsTemplate.replace(/ #{dogsList}/g, dogsListHtml)
+
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/html')
+      return res.end(htmlDogs)
     }
 
     // Phase 2: GET /dogs/new
@@ -98,10 +106,10 @@ const server = http.createServer((req, res) => {
 
     // Phase 3: GET /dogs/:dogId
     if (req.method === 'GET' && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/');
+      const urlParts = req.url.split('/')
       if (urlParts.length === 3) {
-        const dogId = urlParts[2];
-        const dog = dogs.find(dog => dog.dogId === Number(dogId));
+        const dogId = urlParts[2]
+        const dog = dogs.find((dog) => dog.dogId === Number(dogId))
         // Your code here
       }
     }
@@ -113,36 +121,35 @@ const server = http.createServer((req, res) => {
 
     // Phase 5: GET /dogs/:dogId/edit
     if (req.method === 'GET' && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/');
+      const urlParts = req.url.split('/')
       if (urlParts.length === 4 && urlParts[3] === 'edit') {
-        const dogId = urlParts[2];
-        const dog = dogs.find(dog => dog.dogId === Number(dogId));
+        const dogId = urlParts[2]
+        const dog = dogs.find((dog) => dog.dogId === Number(dogId))
         // Your code here
       }
     }
 
     // Phase 6: POST /dogs/:dogId
     if (req.method === 'POST' && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/');
+      const urlParts = req.url.split('/')
       if (urlParts.length === 3) {
-        const dogId = urlParts[2];
-        const dog = dogs.find(dog => dog.dogId === Number(dogId));
+        const dogId = urlParts[2]
+        const dog = dogs.find((dog) => dog.dogId === Number(dogId))
         // Your code here
       }
     }
 
     // No matching endpoint
-    const htmlPage = fs.readFileSync("./views/error.html", 'utf-8');
-    const resBody = htmlPage
-      .replace(/#{message}/g, 'Page Not Found');
+    const htmlPage = fs.readFileSync('./views/error.html', 'utf-8')
+    const resBody = htmlPage.replace(/#{message}/g, 'Page Not Found')
 
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/html");
-    res.write(resBody);
-    return res.end();
-  });
-});
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'text/html')
+    res.write(resBody)
+    return res.end()
+  })
+})
 
-const port = 5000;
+const port = 5000
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log('Server is listening on port', port))
